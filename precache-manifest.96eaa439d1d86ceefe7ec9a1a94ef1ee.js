99d77ac9362181f825be5f73a3df89dee783b5d2
MouradM <wassakh@gmail.com>
2022-02-11 09:54:59 +0100

    Updates

---
+++ precache-manifest.96eaa439d1d86ceefe7ec9a1a94ef1ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ede4308f5e8c9997ee7fd30f1f955ae",
+    "url": "/whitelistsystem/index.html"
+  },
+  {
+    "revision": "76c91adc2ad2e1ce4c97",
+    "url": "/whitelistsystem/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "19cf64bfffa0e54d16bb",
+    "url": "/whitelistsystem/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "76c91adc2ad2e1ce4c97",
+    "url": "/whitelistsystem/static/js/2.4ca16ebd.chunk.js"
+  },
+  {
+    "revision": "19cf64bfffa0e54d16bb",
+    "url": "/whitelistsystem/static/js/main.c9e26815.chunk.js"
+  },
+  {
+    "revision": "4414a7dec43767c0a975",
+    "url": "/whitelistsystem/static/js/runtime-main.2c110a91.js"
+  }
+]);
